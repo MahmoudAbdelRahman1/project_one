61a98e4f1ecc90fbe190d032c1205c62294cb903
mahmoud abdelrahman <mahmoud108abdelrahman@gmamil.com>
2020-11-17 20:01:58 +0200

    Google search

---
+++ js/min.js
@@ -0,0 +1,6 @@
+var boxMin = document.getElementById('conterJs'),
+    buttonSearch = document.getElementById('butjs');
+
+   boxMin.onclick = function clicking(){
+        boxMin.classList.add('mask');
+    }
